43b2fe693a4df4fc078b4bac06a5da20061e2781
Brent Liang <brentliang@brents-air.lan>
2017-12-02 12:01:33 +0630

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Brent Liang on 2/12/17.
+//  Copyright © 2017 Brent Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
