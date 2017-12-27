d046ed92f7311fa2ba922c6ad9afba2474caab7e
Brent Liang <brentliang@Brents-Air.lan>
2017-12-27 11:44:39 +0630

    Finalize changes

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,10 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+func shortNameFromName (_ name: String) -> String {
+    var shortName = name.lowercased()
+    let vowelSet = CharacterSet.init(charactersIn: "aeiou")
+    let range = shortName.rangeOfCharacter(from: vowelSet)
+    if let range = range {
+        shortName = shortName.substring(from: range.lowerBound)
+    }
+    print(shortName)
+    return shortName
+}
 
+func lyricsForName (fullName: String) -> String {
+    let lyricsOutput = [
+        "\(fullName), \(fullName), Bo B\(shortNameFromName(fullName))",
+        "Banana Fana Fo F\(shortNameFromName(fullName))",
+        "Me My Mo M\(shortNameFromName(fullName))",
+        "\(fullName)"].joined(separator: "\n")
+    return lyricsOutput
+}
+
+// MARK: - UITextFieldDelegaet
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +54,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+        
+    }
 
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        if (nameField.text?.isEmpty)! {
+            lyricsView.text = "Please enter your name!"
+        }
+        else {
+            lyricsView.text = lyricsForName(fullName: nameField.text!)
+            
+        }
+    
+    }
+    
 }
 
